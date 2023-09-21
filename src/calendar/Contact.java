ca33a802a42cd62ee794db83f4f238009afe07b9
kenrickeagar <kenrickaeagar@gmail.com>
2023-09-21 14:19:17 -0400

    test message

--- src/calendar/Contact.java
+++ src/calendar/Contact.java
@@ -3,7 +3,31 @@ package calendar;
 public class Contact {
     private Department department;
     private String email;
-    public boolean isValid() {
-        return false;
+
+    public static final String CS_EMAIL = "cs@rutgers.edu", ITI_EMAIL = "iti@rutgers.edu";
+    public static final String BAIT_EMAIL = "bait@rutgers.edu", EE_EMAIL = "ee@rutgers.edu";
+    public static final String MATH_EMAIl = "math@rutgers.edu";
+
+
+    //Contact Constructor
+    public Contact(Department department, String email){
+        this.department = department;
+        this.email = email;
     }
+
+    public boolean validEmail(String email){
+        if(!email.equals(EE_EMAIL) && !email.equals(CS_EMAIL) && !email.equals(BAIT_EMAIL) && !email.equals(MATH_EMAIl) && !email.equals(ITI_EMAIL)){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isValid(Department department, String email) {
+        if(!validEmail){
+            return false;
+        }
+        return true;
+    }
+
 }
+ //test
