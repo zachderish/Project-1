cd270a5299c3e4b4725ffcc664552f13d10f756a
Zachary <zderish@gmail.com>
2023-09-25 09:20:25 -0400

    Add attributes, constructor and comments to Department enum

--- src/calendar/Department.java
+++ src/calendar/Department.java
@@ -1,13 +1,25 @@
 package calendar;
 
+/**
+ * Define the enum class Department
+ * @author Kenrick Eagar, Zachary Derish
+ */
 public enum Department{
 
-    CS, EE, MATH, ITI, BAIT;
+    CS ("Computer Science"),
+    EE ("Electrical Engineering"),
+    MATH ("Information Technology Informatics"),
+    ITI ("Mathematics"),
+    BAIT ("Business Analytics and Information Technology");
 
-    public static void main(String[] args){
-        Department test = Department.CS;
-        Department test1 = Department.EE;
-        System.out.print(test.compareTo(test1));
+    private final String fullName;
+
+    /**
+     * Department constructor
+     * @param fullName the full department name
+     */
+    Department(String fullName) {
+        this.fullName = fullName;
     }
 
 }
