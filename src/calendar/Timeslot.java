43c51a12977855512c5a700eba6a60820a96e1c9
Zachary <zderish@gmail.com>
2023-09-25 09:00:10 -0400

    Add attributes, constructor and comments to Timeslot enum

--- src/calendar/Timeslot.java
+++ src/calendar/Timeslot.java
@@ -1,5 +1,24 @@
 package calendar;
 
+
+/**
+ * Define the enum class Timeslot
+ * @author Kenrick Eagar, Zachary Derish
+ */
 public enum Timeslot{
-        MORNING, AFTERNOON, EVENING
+        MORNING (8, 30),
+        AFTERNOON (12, 30),
+        EVENING (3, 30);
+
+        private final int startHour;
+        private final int startMinute;
+
+
+    /**
+     * Timeslot constructor
+     */
+    Timeslot (int startHour, int startMinute) {
+            this.startHour = startHour;
+            this.startMinute = startMinute;
+        }
     }
