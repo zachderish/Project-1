50cd47825c6f13f6959a169bad1434887f26ceaf
kenrickeagar <125171930+kenrickeagar@users.noreply.github.com>
2023-09-29 23:49:17 -0400

    Added comments

--- src/calendar/EventCalendar.java
+++ src/calendar/EventCalendar.java
@@ -2,18 +2,32 @@ package calendar;
 
 import calendar.Event;
 
+/**
+ * Array-based implementation to hold list of events
+ * @author Kenrick Eagar, Zachary Derish
+ */
+
 public class EventCalendar {
     private Event[] events; //the array holding the list of events
     private int numEvents; //current number of events in the array
     public static final int NOT_FOUND = -1;
 
-    //EventCalendar Constructor
+ /**
+ * EventCalendar Two-Parameter Constructor
+ * @param array of event objects 
+ * @param integer representing the number of events 
+ */
     public EventCalendar(Event[] events, int numEvents) {
         this.events = new Event[4]; //array starts with initial capacity of 4
         int initialSize = 0;
         this.numEvents = initialSize;
     }
-
+    
+/**
+ * Given an event, method will return integer representing the index of the event in the calendar after finding an event that matches particular elements of event
+ * @param event object method that will be searched for
+ * @return integer representing the index of the event in the calendar
+ */
     private int findByElements(Event event){
         Date date = event.getDate();
         Location location = event.getLocation();
@@ -27,6 +41,12 @@ public class EventCalendar {
         return NOT_FOUND;
 
     }
+
+/**
+ * Given an event, method will return integer representing the index of the matched event in the calendar
+ * @param event object method that will be searched for
+ * @return integer representing the index of the event in the calendar
+ */
     private int find(Event event) {//find the index of the event;
         int n = this.numEvents;
         for (int i = 0; i < n; i++) {
@@ -41,7 +61,11 @@ public class EventCalendar {
         }
         return NOT_FOUND; //did not find list
     }
-
+/**
+ * Void method that will grow EventCalendar events array by 4
+ * Method will reassign calendars events array to an array of larger capacity with the same event objects
+ * 
+ */
     private void grow() {
         int n = events.length;
         Event[] temp = new Event[n + 4]; //increase capacity by 4
@@ -51,7 +75,10 @@ public class EventCalendar {
 
         this.events = temp; //since they are the same size, we can simply set equal to copy elements
     }
-
+/**
+ * Method will search array for a null value indicating an empty space in the array and will return boolean indicating if there is a null index
+ * @return will return a boolean indicating true if array has space to add objects (has found a null index), false otherwise
+ */
     private boolean hasSpace() {
         for (int i = 0; i < events.length; i++) {
             if (events[i] == null) { //if theres a spot where object = null then it has space to add one
@@ -61,7 +88,10 @@ public class EventCalendar {
 
         return false;
     }
-
+/**
+ * @param event object that will be added to events array/EventCalendar
+ * @return will return a boolean indicating true if event was successfully added to calendar, false otherwise
+ */
     public boolean add(Event event) {
         if (!hasSpace()) {
             grow(); //if the array does not have space we have to resize it
@@ -96,7 +126,10 @@ public class EventCalendar {
         return false;
 
     }
-
+/**
+ * @param event object that we will be searching for in array
+ * @return will return a boolean indicating true if event was found in calendar, false otherwise
+ */
     public boolean contains(Event event) {
         int didWeFindIt = this.find(event);
         if (didWeFindIt == NOT_FOUND) { //if we cant find it in the list then list does not contain it
@@ -105,7 +138,10 @@ public class EventCalendar {
         return true;
     }
 
-
+/**
+ * void method that will print out the eventCalendar
+ * 
+ */
     public void print() {
         System.out.println("* Event calendar *");
         for (int i = 0; i < this.numEvents; i++) {
@@ -115,7 +151,10 @@ public class EventCalendar {
     }
 
 
-
+/**
+ * Method that will print out sorted events by date using insertion sort
+ * 
+ */
     public void printByDate() {
         System.out.println("* Event calendar by event date and start time *");
         if(this.numEvents > 1) { // Insertion sort from: https://www.geeksforgeeks.org/insertion-sort/
@@ -156,7 +195,10 @@ public class EventCalendar {
         }*/
         return location1.compareTo(location2) > 0;
     }
-
+/**
+ * Method that will print out sorted events by campus using insertion sort
+ * 
+ */
     public void printByCampus() {
         System.out.println("* Event calendar by campus and building *");
         if(this.numEvents > 1) { // Insertion sort from: https://www.geeksforgeeks.org/insertion-sort/
@@ -179,6 +221,10 @@ public class EventCalendar {
 
     } //ordered by campus and building/room
 
+/**
+ * Method that will print out sorted events by department using insertion sort
+ * 
+ */
     public void printByDepartment() {
         System.out.println("* Event calendar by department *");
         if(this.numEvents > 1) { // Insertion sort from: https://www.geeksforgeeks.org/insertion-sort/
@@ -200,10 +246,19 @@ public class EventCalendar {
         System.out.println("* end of event calendar *");
     } //ordered by department
 
+
+/**
+ * @return integer indicating the current number of events in the events calendar
+ * 
+ */
     public int getNumEvents() {
         return this.numEvents;
     }
 
+ /**
+ * testbed main used to test methods in eventCalendar class
+ * 
+ */
     public static void main(String[] args) {
         Date date1 = new Date(2024, 2, 14);
         Contact contact1 = new Contact(Department.CS, "cs@rutgers.edu");
